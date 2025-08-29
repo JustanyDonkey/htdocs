4f789491fe92e14d6339ec8f41a1347b94c2c4fe
JustanyDonkey <heitor.pineiro.s@gmail.com>
2025-08-28 22:22:39 -0300

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,14 @@
 <?php
+header('Access-Control-Allow-Origin: http://localhost');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+
+// Responder imediatamente para requisições OPTIONS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit(0);
+}
 // Caminho CORRETO - vendor está na raiz do myAPI
 require_once __DIR__ . "/myAPI/vendor/autoload.php";
 
